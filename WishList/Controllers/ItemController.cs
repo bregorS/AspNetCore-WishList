83599b902d34f0671de7290b69db7bff9b210f70
Roger Brooks <roger.brooks@creative-technologies.co.uk>
2018-08-31 14:21:11 +0100

    Add item actions

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿namespace WishList.Controllers
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using WishList.Data;
+
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Items.ToList();
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(i => i.Id == id);
+
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
