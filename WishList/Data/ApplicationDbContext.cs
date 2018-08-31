36eac450f6f3f14021171579b90b8c5e9aecd0ce
Roger Brooks <roger.brooks@creative-technologies.co.uk>
2018-08-31 11:56:03 +0100

    Add Item model code

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace WishList.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using WishList.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
