30ec4015c8ed297d12a7aafa321774d291b2f090
Roger Brooks <roger.brooks@creative-technologies.co.uk>
2018-08-31 11:37:22 +0100

    Add error and index view and controller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace WishList.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
+    }
+}
